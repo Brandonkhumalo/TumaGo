81f11188bbe4b6fd0129f9a3faf7ca5fe5864dc6
Brandon <Apex2.0predator@gmail.com>
2025-08-11 18:19:01 +0200

    Stop tracking TumaGo_Backend folder and add to .gitignore

--- TumaGo_Backend/TumaGo_Server/serializers/driverSerializer/rideSerializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from ...models import Delivery
-
-class DeliverySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Delivery
-        fields = ['origin_lat', 'origin_lng', 'destination_lat', 'destination_lng', 'vehicle', 'fare',
-                  'payment_method', 'delivery_id', 'date' ]
