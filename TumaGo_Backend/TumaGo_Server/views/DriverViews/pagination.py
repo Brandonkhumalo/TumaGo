359026b1270f15b74dcad19da5b43e912d530802
Brandon <Apex2.0predator@gmail.com>
2025-08-11 18:19:01 +0200

    Stop tracking TumaGo_Backend folder and add to .gitignore

--- TumaGo_Backend/TumaGo_Server/views/DriverViews/pagination.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.pagination import CursorPagination
-
-class DeliveryCursorPagination(CursorPagination):
-    page_size = 5
-    ordering = '-start_time'  # must match a unique or indexed field
